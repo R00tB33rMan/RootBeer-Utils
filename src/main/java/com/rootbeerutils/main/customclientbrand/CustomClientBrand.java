c83114a52c0e59d278d3e884ca4f510fec708be7
corven <corvenzett@gmail.com>
2026-08-04 18:07:18 +0200

    feat(utils): Add ModMenu Support

--- src/main/java/com/rootbeerutils/main/customclientbrand/CustomClientBrand.java
+++ src/main/java/com/rootbeerutils/main/customclientbrand/CustomClientBrand.java
@@ -7,7 +7,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CustomClientBrand implements ModInitializer {
+public class CustomClientBrand  implements ModInitializer {
 
     public static final String MOD_ID = "ClientBrand";
     private static final Logger LOGGER = LoggerFactory.getLogger("ClientBrand-RBU");
@@ -20,5 +20,4 @@ public class CustomClientBrand implements ModInitializer {
         AutoConfig.register(CCBConfig.class, GsonConfigSerializer::new);
         LOGGER.info(MOD_ID + " Loaded!");
     }
-
 }
